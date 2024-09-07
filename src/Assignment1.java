f4c51e988f2d8309747ad4a477534031383ad9d0
Elijah <etucker210@gmail.com>
2024-09-07 16:30:41 -0400

    Create calculateDifference function

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -19,6 +19,7 @@ import java.io.Serial;
  * 07 Sep 2024 3:00PM | Create File
  * 07 Sep 2024 3:12PM | Create main functions
  * 07 Sep 2024 3:41PM | Create calculate sum function
+ * 05 Sep 2024 4:29PM | Create calculateDifference function
  *
  ************************************************************************/
 
@@ -41,7 +42,10 @@ public class Assignment1 {
         int number1 = 5;
         int number2 = 3;
 
+        System.out.printf("Number 1 is: %d\nNumber 2 is: %d\n", number1, number2);
+
         System.out.printf("The sum of the two numbers is: %d\n", calculateSum(number1, number2));
+        System.out.printf("Number 1 minus Number 2 is: %d\n", calculateDifference( number1, number2) );
     }
 
     /*********************************************************************
@@ -58,6 +62,23 @@ public class Assignment1 {
     public static int calculateSum( int number1, int number2 ) {
 
         return number1 + number2; //add the numbers and return the value
+
+    }
+
+    /*********************************************************************
+     * Function Name: calculateDifference
+     * Inputs:        number1 - int - the first number to be subtracted
+     *                number2 - int - the second number to be subtracted
+     *
+     * Output:        int - the subtraction of the two numbers
+     *
+     * Description:   This function subtracts two numbers
+     *
+     ********************************************************************/
+
+    public static int calculateDifference( int number1, int number2 ) {
+
+        return number1 - number2; //subtract the numbers and return
 
     }
 }
