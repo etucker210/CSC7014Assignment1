2c966d516419b5d4fac31fd160886b9638182cec
Elijah <etucker210@gmail.com>
2024-09-07 15:01:31 -0400

    Create Files

---
+++ src/Assignment1.java
@@ -0,0 +1,24 @@
+/*************************************************************************
+ *
+ * File name: Assignment1.java
+ * Author:    Elijah Tucker
+ * Last Edit: 7 Sep 2024
+ *
+ * Prompt:    Write a Java program that preforms basic arithmetic
+ *            operations (addition, subtraction, multiplication, and
+ *            division). Adhere to a custom coding style guide, focusing
+ *            on naming conventions, consist use of expressions and
+ *            statements and including comments for clarity.
+ *
+ * Change Log:
+ *
+ * Date & Time        | Description
+ * -------------------|-------------
+ * 07 Sep 2024 3:00PM | Create File
+ *
+ ************************************************************************/
+
+public class Assignment1 {
+
+
+}
